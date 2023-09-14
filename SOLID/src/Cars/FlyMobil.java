73e11f64616c7e3be7efd3e405d1bf6ccc02aa08
Anna_Barsukova <barsukoba.a1990@yandex.ru>
2023-09-15 01:12:15 +0300

    SOLID step 6

---
+++ SOLID/src/Cars/FlyMobil.java
@@ -0,0 +1,25 @@
+package Cars;
+// такое наследование нарушает принцип LSP
+public class FlyMobil extends Car{
+
+    /**
+     * Конструктор класса Car
+     *
+     * @param brand         марка
+     * @param model         модель
+     * @param exteriorColor цвет кузова
+     * @param bodyType      тип кузова
+     * @param wheelsCount   число колес
+     * @param fuelType      тип топлива
+     * @param gearboxType   тип коробки передач
+     * @param engine        объем двигателя
+     */
+    public FlyMobil(String brand, String model, String exteriorColor, String bodyType, int wheelsCount, String fuelType, String gearboxType, int engine) {
+        super(brand, model, exteriorColor, bodyType, wheelsCount, fuelType, gearboxType, engine);
+    }
+
+    @Override
+    protected void drive(String gearboxType) {
+        System.out.println("Эта машина умеет летать, но не умеет ехать");
+    }
+}
